d6b85a008929ce7a08571a0f2357249fdc2648be
d0ughnat <nledesma35@gmail.com>
2025-08-18 22:47:20 +0800

    file put inside the pages

--- resources/js/pages/About.tsx
+++ resources/js/pages/About.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import HeaderBlack from '@/components/HeaderBlack';
 import { Footer } from '@/components/Footer';
-import Hero from '@/AboutUs/Hero';
-import MissionandVision from '@/AboutUs/MissionandVision';
-import Team from '@/AboutUs/Team';
+import Hero from '@/pages/AboutUs/Hero';
+import MissionandVision from '@/pages/AboutUs/MissionandVision';
+import Team from '@/pages/AboutUs/Team';
 import { TestimonialsSection } from '@/pages/landingpage/TestimonialsSection';
 import { TrustedCompaniesSection } from '@/pages/landingpage/TrustedCompaniesSection';
 import { ContactSection } from '@/pages/landingpage/ContactSection';
