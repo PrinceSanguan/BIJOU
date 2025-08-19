8891e9a28f0fc3e48545980aa76a466742f9a531
d0ughnat <nledesma35@gmail.com>
2025-08-19 23:10:17 +0800

    aboutus fix ui

--- resources/js/pages/Blog.tsx
+++ resources/js/pages/Blog.tsx
@@ -8,17 +8,24 @@ import AboutUs from '@/pages/Blogs/AboutUs';
 import { TrustedCompaniesSection } from '@/pages/landingpage/TrustedCompaniesSection';
 import { TestimonialsSection } from '@/pages/landingpage/TestimonialsSection';
 
-
 export default function Blog() {
   return (
     <>
       <HeaderBlack />
-      <main className="container mx-auto py-12" role="main">  
-        <Hero />
-        <Articles />
-        <AboutUs />
-        <TestimonialsSection />
-        <TrustedCompaniesSection />
+      <main role="main">  
+        <div className="container mx-auto py-12">
+          <Hero />
+          <Articles />
+        </div>
+
+        <div className="justify-center items-center">
+          <AboutUs />
+        </div>
+      
+        <div className="container mx-auto py-12">
+          <TestimonialsSection />
+          <TrustedCompaniesSection />
+        </div>
       </main>
       <Footer />
     </>
